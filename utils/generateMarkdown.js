a690f7e7ef192d6b1c984013624746e8ce24b1a0
dalabi91 <152018805+dalabi91@users.noreply.github.com>
2024-02-20 16:20:05 +0000

    added founction to generate markdown data

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,44 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ## Table of Contents
+
+[Description] (#description)
+[Installation] (#installation)
+[Usage] (#usage)
+[License] (#license)
+[Contributing] (#contributing)
+[Tests] (#test)
+[Questions] (#test)
+
+  ## Installation
+  ${data.installation}
+
+  ## Usage
+  ${data.usage}
+
+  ## License
+  ${data.license}
+
+  ![GitHub license](https://img.shields.io/badge/license-${data.license}-blue.svg)
+  This product is covered by the ${data.license} license.
+
+  ## Contributing
+  ${data.contributing}
+
+  ## Test
+  ${data.test}
+
+  ## Questions
+  Should you have any question regarding this project, feel free to contact through email or visit my github account
+  <a href="mailto:${data.email}">${data.email}</a>
+  <a href="https://github.com/${data.github}">${data.github}</a>
 
 `;
 }
